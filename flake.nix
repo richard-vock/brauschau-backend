f504dcb80607e9e1bb2b92347678713b5e60aa85
Richard Vock <nrtn@posteo.net>
2024-08-20 18:09:47 +0200

    initial commit

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "dev setup";
+
+  outputs = { nixpkgs, ... }:
+  let
+    inherit (nixpkgs) lib;
+
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    npm = pkgs.nodePackages;
+  in
+  {
+    devShells.x86_64-linux.default =
+      pkgs.mkShell {
+        packages = with npm; [
+          pnpm
+        ];
+      };
+  };
+}
